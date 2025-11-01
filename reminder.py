e0c700faf2dd369d28c0d351ea17883bf0fb5c64
Rob Michael <robmichael93@gmail.com>
2025-10-31 23:10:04 -0500

    Chapter 5 commit
    Had to revert to an earlier version of tox to be able to run the coverage module.  Also had to fix a small style error in the main file, reminder.py.  Changed the TOML file back to testing Python 3.13, which is what I have installed on my machine.

--- reminder.py
+++ reminder.py
@@ -54,7 +54,7 @@ def _find_match(target: str, task_list: List[Task]):
             potential_match.append((score, task.name))
     if potential_match:
         potential_match = sorted(potential_match, key=lambda x: x[0], reverse=True)
-        click.echo(f"Cannot find {target}, here are the close matches:")
+        click.echo(f"Cannot find {target}, here are the close matches: ")
         for num, match in enumerate(potential_match):
             click.echo(f"{num+1}. {match[1]}")
 
